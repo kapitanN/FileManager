fe10996f394f1a2f978dc983de69b5bff5024046
nikita <nikita.shapovalov.94@gmail.com>
2015-12-19 15:45:16 +0200

    pract2

---
+++ pract2/src/Tree.java
@@ -0,0 +1,12 @@
+/**
+ * Created by nikita on 18.12.15.
+ */
+
+import com.sanny.Algo;
+
+public class Tree extends Algo{
+    public static void main(String args[]){
+        System.out.println("hi");
+        Algo algo = new Algo();
+    }
+}
