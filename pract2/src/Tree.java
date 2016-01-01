a4b17d2774003632d4b30f63e5da225a5ea3e7dd
Nikita <nikita.shapovalov.94@gmail.com>
2016-01-02 01:15:36 +0200

    02.01.15

--- pract2/src/Tree.java
+++ pract2/src/Tree.java
@@ -2,11 +2,13 @@
  * Created by nikita on 18.12.15.
  */
 
-import com.sanny.Algo;
+//import com.sanny.Algo;
+//import com.sanny.API;
 
-public class Tree extends Algo{
+public class Tree{
     public static void main(String args[]){
         System.out.println("hi");
-        Algo algo = new Algo();
+       // Algo algo = new Algo();
+        //System.out.println(API.generateTree());
     }
 }
---
+++ test/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+import com.sanny.Algo;
+import com.sanny.API;
+
+
+
+public class Main{
+    public static void main(String args[]){
+        System.out.println("hi");
+//        Algo algo = new Algo();
+        System.out.println(API.generateTree());
+    }
+}
+
