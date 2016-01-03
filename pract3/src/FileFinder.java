27185c48b54abc06aea3fdef672c9cfdbaeee1ba
nikita <nikita.shapovalov.94@gmail.com>
2016-01-03 14:47:59 +0200

    03.01.16

---
+++ pract3/src/FileFinder.java
@@ -0,0 +1,11 @@
+
+import java.io.File;
+import java.util.*;
+
+/**
+ * Created by nikita on 03.01.16.
+ */
+public class FileFinder {
+    List<File> fileList = new ArrayList<File>();
+    String path = "/media/nikita/D/Study/Insart/pract3";
+}
---
+++ pract3/src/FilePath.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+/**
+ * Created by nikita on 03.01.16.
+ */
+public class FilePath {
+
+    public static void main(String ... args){
+        FileFinder fileFinder = new FileFinder();
+    }
+}
